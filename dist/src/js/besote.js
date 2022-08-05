591c3f1e126d7542256f6e4f52f09dab1973c9da
lexus <erickllaureano@gmail.com>
2022-08-05 01:06:09 -0500

    first commit

---
+++ dist/src/js/besote.js
@@ -0,0 +1,19 @@
+//!addcom !besote $(eval var origin="$(user)"; var destination="$(touser)"; $(urlfetch json [URL]))
+var probability = Math.floor(Math.random() * 100);
+if (probability === 0) {
+    ( "@" + origin + " intento darle un beso a @" + destination + " pero recibió una cachetada!");
+}
+else if (probability <= 10) {
+    ( "@" + origin + " agarro valor y le robo un besote a @" + destination);
+}
+else if (probability <= 50) {
+    ( "@" + origin + " quiere jugar con fuego y comerse a besos a @" + destination + " . lo lograra??");
+}
+else if (probability <= 80) {
+    ( "@" + origin + " le planto tremendo besote a @" + destination);
+}
+else if (probability <= 98) {
+    ( "@" + origin + " se arriesga con todo y le da su mejor beso a @" + destination);
+} else {
+    ( "@" + origin + " ha logrado enamorar a @" + destination + " con un beso súper apasionado");
+}
