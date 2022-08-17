c1ead4ea3604baa0a71efc9c484ae655661fd0fe
lexus <erickllaureano@gmail.com>
2022-08-17 01:17:49 -0500

    fix probability and copy

--- dist/src/js/besote.js
+++ dist/src/js/besote.js
@@ -1,12 +1,12 @@
 var probability = Math.floor(Math.random() * 100);
-if (probability === 0) {
+if (probability <= 2) {
     ( "@" + origin + " intento darle un beso a @" + destination + " pero recibió una cachetada!");
 }
 else if (probability <= 10) {
     ( "@" + origin + " agarro valor y le robo un besote a @" + destination);
 }
 else if (probability <= 50) {
-    ( "@" + origin + " quiere jugar con fuego y comerse a besos a @" + destination + " . lo lograra??");
+    ( "@" + origin + " quiere jugar con fuego y comerse a besos a @" + destination + " ¿lo lograra?");
 }
 else if (probability <= 80) {
     ( "@" + origin + " le planto tremendo besote a @" + destination);
